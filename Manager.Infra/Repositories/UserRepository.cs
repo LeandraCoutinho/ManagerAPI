57cb1967e57a45471a7ca0e817b09c7b50cfe94e
Leandra Coutinho <leandracou123@gmail.com>
2024-06-06 22:28:42 -0300

    estruturando a camada de infraestrutura

---
+++ Manager.Infra/Repositories/UserRepository.cs
@@ -0,0 +1,74 @@
+using Manager.Domain.Entities;
+using Manager.Infra.Context;
+using Manager.Infra.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Manager.Infra.Repositories;
+
+public class UserRepository : BaseRepository<User>, IUserRepository
+{
+    private readonly ManagerContext _context;
+
+    public UserRepository(ManagerContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public override async Task<User> Update(User user)
+    {
+        _context.Entry(user).State = (Microsoft.EntityFrameworkCore.EntityState)EntityState.Modified;
+        await _context.SaveChangesAsync();
+
+        return user;
+    }
+
+    public override async Task<User> Get(long id)
+    {
+        var user = await _context.Users.Where<User>(u => u.Id == id).AsNoTracking().ToListAsync();
+        return user.FirstOrDefault();
+    }
+
+    public override async Task<List<User>> GetTask()
+    {
+        var alluser = await _context.Users.AsNoTracking().ToListAsync();
+        return alluser;
+    }
+
+    public async Task<User> GetByEmail(string email)
+    {
+        var user = await _context.Users.
+            Where
+            (
+                x => x.Email.ToLower() == email.ToLower()
+            )
+            .ToListAsync();
+
+        return user.FirstOrDefault();
+    }
+
+    public async Task<List<User>> SearchByEmail(string email)
+    {
+        var allUsers = await _context.Users
+            .Where
+            (
+                x => x.Email.ToLower().Contains(email.ToLower())
+            )
+            .AsNoTracking()
+            .ToListAsync();
+
+        return allUsers;
+    }
+
+    public async Task<List<User>> SearchByName(string name)
+    {
+        var allUsers = await _context.Users
+            .Where
+            (
+                x => x.Email.ToLower().Contains(name.ToLower())
+            )
+            .AsNoTracking()
+            .ToListAsync();
+
+        return allUsers;
+    }
+}
